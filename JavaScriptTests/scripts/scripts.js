2d7d895654f10dbc64860ec58487054e431a0aed
ThisseasX <thisseasx@yahoo.com>
2018-01-25 19:20:24 +0200

    Added experiments

--- JavaScriptTests/scripts/scripts.js
+++ JavaScriptTests/scripts/scripts.js
@@ -1,18 +1,25 @@
-var a = false;
+var hidden = false;
 
 function toggleVisibility() {
-  if (a) {
+  if (hidden) {
     document.getElementById("myheader").style.visibility = "visible";
   } else {
     document.getElementById("myheader").style.visibility = "hidden";
   }
-  a = !a;
+  hidden = !hidden;
+}
+
+function createParagraph(value) {
+  let text = document.createTextNode(value);
+  let paragraph = document.createElement("p");
+  paragraph.appendChild(text);
+  document.body.appendChild(paragraph);
 }
 
 function case1() {
   var myvar = 'myvalue';
   (function () {
-    document.write(myvar);
+    createParagraph(myvar);
     var myvar = 'local value';
   })();
 }
@@ -21,7 +28,27 @@ function case2() {
   var myothervar = 'my value';
   (function () {
     var myothervar;
-    document.write(myothervar);
+    createParagraph(myothervar);
     myothervar = 'local value';
   })();
+}
+
+function booleanTest() {
+  var b = new Boolean(false);
+  if (b == true) {
+    createParagraph("TRUE");
+  } else {
+    createParagraph("FALSE");
+  }
+}
+
+function assignmentTest() {
+  var x = 1;
+  var y = 4;
+  if ((x = y)) {
+    createParagraph('TRUE');
+  } else {
+    createParagraph('FALSE');
+  }
+  createParagraph("x = " + x + " and y = " + y);
 }
